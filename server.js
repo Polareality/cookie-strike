9e1d040b670f719803300588be5e4f878c1f3c7e
Polareality <78053812+Polareality@users.noreply.github.com>
2024-11-15 14:30:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -105,6 +105,6 @@ app.post('/summarize', async (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
